e711f9adfb0785586f81c9ea5630fde23bbf728c
teodor8001 <teoflorin02@gmail.com>
2025-07-14 02:27:17 +0300

    Implemented app config and modelMapper

--- src/main/java/com/mindhive/api/services/UserService.java
+++ src/main/java/com/mindhive/api/services/UserService.java
@@ -4,14 +4,21 @@ import com.mindhive.api.daos.UserRequestDAO;
 import com.mindhive.api.entity.User;
 import com.mindhive.api.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
     private UserRepository userRepository;
+    private ModelMapper modelMapper;
 
     public void createUser(UserRequestDAO userRequest) {
-        //TODO
+        User newUser = modelMapper.map(userRequest, User.class);
+        userRepository.save(newUser);
+    }
+
+    public User findUser(Long userId) {
+        return userRepository.findById(userId).orElseThrow();
     }
 }
